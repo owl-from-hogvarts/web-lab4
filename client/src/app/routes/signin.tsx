b432c6f6c761821b2f2651d56984d24784b49b0d
owl-from-hogvarts <transftank@mail.ru>
2023-12-29 18:44:26 +0300

    feat: implement sign-in logic

--- client/src/app/routes/signin.tsx
+++ client/src/app/routes/signin.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { LoginContainer, MIN_LENGTH, TLoginData } from "./login-data";
+import { LoginContainer, MIN_LENGTH } from "./login-data";
 import Field, { FieldInput } from "app/components/field";
 import Button from "app/components/button";
 import { useForm } from "react-hook-form";
+import { TLoginData, registerUser } from "app/api/users";
+import Title from "app/components/title";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function () {
+  const nav = useNavigate()
+  
   const {register, handleSubmit, formState: {isDirty, isValid}} = useForm<TLoginData>({
     defaultValues: {
       password: "",
@@ -14,18 +19,20 @@ export default function () {
   
   return (
     <LoginContainer>
+      <Title>Register</Title>
       <Field label="Login">
         <FieldInput {...register("login", {
           minLength: MIN_LENGTH
         })}/>
       </Field>
       <Field label="password" >
-        <FieldInput {...register("password", {minLength: MIN_LENGTH})}/>
+        <FieldInput type="password" {...register("password", {minLength: MIN_LENGTH})}/>
       </Field>
+      <Link to="/login" >Already have an account? Click to login</Link>
       <Button className="rounded" style={{ width: "100%" }} onClick={handleSubmit(async (data) => {
-        // if (isDirty && isValid) {
-        //   fetch(USER_ENDPOINT_URL, {method: "POST", body: JSON.stringify(data)})
-        // }
+        if (isDirty && isValid) {
+         registerUser(nav)(data)
+        }
       })}>
         Sign in
       </Button>

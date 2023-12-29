aabeb0698b2ab315e534da7e92c2982031859a67
owl-from-hogvarts <transftank@mail.ru>
2023-12-29 18:42:28 +0300

    feat: store user token in local storage

--- client/src/app/auth/auth.ts
+++ client/src/app/auth/auth.ts
@@ -2,10 +2,16 @@
 // impossible to use hooks within router
 // therefore can't use react context api 
 // to store token
+const KEY_USER_TOKEN = "user_token"
 
-let userToken: string | null = null
+let userToken: string | null = window.localStorage.getItem(KEY_USER_TOKEN)
 
-export function setUserToken(token: string) {
+export function setUserToken(token: string | null) {
+  if (token == null) {
+    window.localStorage.removeItem(KEY_USER_TOKEN)
+  } else {
+    window.localStorage.setItem(KEY_USER_TOKEN, token?.toString())
+  }
   userToken = token
 }
 
